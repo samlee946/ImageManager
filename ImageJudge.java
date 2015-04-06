f549cac57ae26da68b9df37face102f99edd8083
wanglunzi <wanglunzi@163.com>
2015-04-07 00:10:35 +0800

    什么都还没改

---
+++ ImageJudge.java
@@ -0,0 +1,28 @@
+package JavaStudy;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+/*
+ *≈–∂œ «≤ª «’’∆¨
+ */
+public class ImageJudge {
+	public static boolean IsImage(File file)
+	{
+		try{
+			BufferedImage bi=ImageIO.read(file);
+			if(bi!=null)
+			{
+				return true;
+			}
+			
+		}catch(Exception e){
+		return false;
+		}
+		return false;
+	}
+
+}
